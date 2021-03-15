8dce00f73b87a86ce6d1b7febd5a8f2505405a81
MuhamedDzevlan91 <37083038+MuhamedDzevlan91@users.noreply.github.com>
2021-03-15 22:13:31 +0100

    ispravka

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,11 +1,14 @@
-Function localScope(){
+function localScope() {
     var myVariable = "I am here!";
+    console.log(myVariable);
 }
 
-if (typeof myVariable != "undefined"){
-    console.log("outside localScope", myVariable)
+localScope();
+if (typeof myVariable != "undefined") {
+    console.log("outside localScope", myVariable);
 } else {
-    console.log("outside localScope UNDEFINED!!!")
+    console.log("outside localScope UNDEFINED!!!");
 }
+
 
 module.exports=localScope;
